4717505f8ff36130e9d050d158acdfee6a4b0810
Matthew Quijano <mq022002@gmail.com>
2024-01-28 04:23:31 -0500

    Change: Documentation regarding layout to draw similarities to Django

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 // https://nextjs.org/learn/dashboard-app/creating-layouts-and-pages#creating-the-dashboard-layout
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-// Pages within the dashboard app/dashboard directory will be nested within this layout.
+// Pages within the dashboard app/dashboard directory will be nested within this layout. This is like a "base.html" template in Django, but if you had one in a specific app.
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
