e464335609464b3d1f778f2b7bee1c7b06c8e179
Matthew Quijano <mq022002@gmail.com>
2024-01-28 11:40:10 -0500

    Add: Data fetching for dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,36 @@
 // https://nextjs.org/learn/dashboard-app/creating-layouts-and-pages#creating-the-dashboard-page
 // URL routing is actually crazy simple. It just takes the structure of your project's files, and maps that to URLs.
-export default function Page() {
-    return <p>Dashboard Page</p>;
-  }
+// export default function Page() {
+//     return <p>Dashboard Page</p>;
+//   }
+
+// https://nextjs.org/learn/dashboard-app/fetching-data#fetching-data-for-the-dashboard-overview-page
+// Page is now an async component, using "await" to fetch data. Commented out code for "Card", "RevenueChart", and "LatestInvoices" will cause errors at the moment.
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+ 
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {/* <RevenueChart revenue={revenue}  /> */}
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+  );
+}
