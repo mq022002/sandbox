94b2b0cb18b7b4788890c019f2c47b242eb090d3
Matthew Quijano <mq022002@gmail.com>
2024-01-28 03:15:37 -0500

    Add: Nested dashboard route segment

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+// https://nextjs.org/learn/dashboard-app/creating-layouts-and-pages#creating-the-dashboard-page
+// URL routing is actually crazy simple. It just takes the structure of your project's files, and maps that to URLs.
+export default function Page() {
+    return <p>Dashboard Page</p>;
+  }
