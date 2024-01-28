6a8dd720227e8fa92b6786f235abffd670eca58e
Matthew Quijano <mq022002@gmail.com>
2024-01-28 01:12:13 -0500

    Add: Global styles to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+// https://nextjs.org/learn/dashboard-app/css-styling#global-styles
+// This adds global CSS styles to your application. Typically best to apply it to a top-level component, similar to the "base.html" in Django projects.
+import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
