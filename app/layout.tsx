00cc043a31cd5aba210ee47ba880a94abc3b791f
Matthew Quijano <mq022002@gmail.com>
2024-01-28 01:45:20 -0500

    Add: Inter as primary font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 // https://nextjs.org/learn/dashboard-app/css-styling#global-styles
 // This adds global CSS styles to your application. Typically best to apply it to a top-level component, similar to the "base.html" in Django projects.
 import '@/app/ui/global.css';
+// https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#adding-a-primary-font
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -9,7 +11,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* Adding the Inter font here means that it will be applied throughout the application. */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+// https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#adding-a-primary-font
+// This imports the Inter font from Google Fonts, with the latin subset.
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
