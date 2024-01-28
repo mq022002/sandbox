2ce2042ada8d63ed0fc64a784745b9fc6b3c3c0c
Matthew Quijano <mq022002@gmail.com>
2024-01-28 04:40:26 -0500

    Add: Functionality so that it highlights active links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,7 @@
+// https://nextjs.org/learn/dashboard-app/navigating-between-pages#pattern-showing-active-links
+// We need to turn nav-links.tsx into a client component. As it standas, usePathname() is a client-side specific feature.
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -6,6 +10,8 @@ import {
 // https://nextjs.org/learn/dashboard-app/navigating-between-pages#the-link-component
 // Allows for client-side navigation between pages, which makes it so that the entire page doesn't have to reload.
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -20,6 +26,7 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
@@ -29,7 +36,13 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            // Through the use of the clsx library that I learning earlier, we can conditionally apply classes to the <Link> component based on the current URL.
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
