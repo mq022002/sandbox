427bf4d51159d2c12ddfbefe9eca5266aa05a352
Matthew Quijano <mq022002@gmail.com>
2024-01-28 01:30:21 -0500

    Add: CSS module implementation of pre-styled black triangle

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
@@ -13,9 +14,12 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* https://nextjs.org/learn/dashboard-app/css-styling#tailwind */}
           {/* This is a copy-paste pre-styled component using Tailwind CSS to create a black triangle. */}
-          <div
+          {/* <div
             className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
-          />
+          /> */}
+          {/* https://nextjs.org/learn/dashboard-app/css-styling#css-modules */}
+          {/* Alternative implementation of the above, using CSS modules instead. */}
+          <div className={styles.shape} />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
