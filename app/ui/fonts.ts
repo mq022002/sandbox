949e3c27b31ea362b03b024ab7b477dc2cf6d129
Matthew Quijano <mq022002@gmail.com>
2024-01-28 02:31:40 -0500

    Add: Lusitana as secondary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,14 @@
 // https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#adding-a-primary-font
 // This imports the Inter font from Google Fonts, with the latin subset.
-import { Inter } from 'next/font/google';
- 
+// import { Inter } from 'next/font/google';
+
+// https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#practice-adding-a-secondary-font
+// I had to reveal the solution for this one... I was able to work the subset, since it was litearlly the same thing as the Inter, but didn't know how to add the weight.
+import { Inter, Lusitana } from 'next/font/google';
+
 export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
