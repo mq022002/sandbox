a2287bcd8c9ddfd3d28a5fa1abdf7861b937e8f7
Matthew Quijano <mq022002@gmail.com>
2024-01-28 03:18:24 -0500

    Add: Nested routes for customers and invoices in dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+// https://nextjs.org/learn/dashboard-app/creating-layouts-and-pages#practice-creating-the-dashboard-pages
+// Easy work.
+export default function Page() {
+    return <p>Customers Page</p>;
+  }
