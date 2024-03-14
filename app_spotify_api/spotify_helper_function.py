ff0107ceff3b746d2b72489fe1aed03b48e1b718
Matthew Quijano <mq022002@gmail.com>
2024-03-14 19:09:56 -0400

    feat: search_for_artist to search for an artist on Spotify API

--- app_spotify_api/spotify_helper_function.py
+++ app_spotify_api/spotify_helper_function.py
@@ -1,7 +1,9 @@
+# https://www.youtube.com/watch?v=WAmEZBEeNmg
+
 import os
 from dotenv import load_dotenv
 import base64
-from requests import post
+from requests import post, get
 import json
 
 load_dotenv()
@@ -13,7 +15,7 @@ secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 def get_token():
     """
-    This gets an access token from the Spotify API using client credentials.
+    Retrieves an access token from the Spotify API using client credentials.
 
     Returns:
         str: The access token.
@@ -35,5 +37,40 @@ def get_token():
     return token
 
 
+def get_auth_header(token):
+    """
+    Returns the authorization header with the provided token.
+
+    Args:
+        token (str): The access token.
+
+    Returns:
+        dict: The authorization header.
+    """
+    return {"Authorization": "Bearer " + token}
+
+
+def search_for_artist(token, artist_name):
+    """
+    Search for an artist on Spotify API.
+
+    Args:
+        token (str): The access token for authentication.
+        artist_name (str): The name of the artist to search for.
+
+    Returns:
+        dict: A dictionary containing the JSON response from the Spotify API.
+
+    """
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)
+    print(json_result)
+
+
 token = get_token()
-print(token)
+search_for_artist(token, "The Beatles")
