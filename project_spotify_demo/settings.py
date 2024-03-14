5dae90adf8e3fed06514a411e47b54ea0934f7fa
Matthew Quijano <mq022002@gmail.com>
2024-03-14 18:08:29 -0400

    refactor: Secure regenerated Django secret key

--- project_spotify_demo/settings.py
+++ project_spotify_demo/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#8jevdg@^8=ge6-yt7!d7814%udx@fw^_w8vgh)yz^84iay7l("
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
