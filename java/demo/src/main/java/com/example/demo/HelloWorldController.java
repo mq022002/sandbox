6d989a0e52a176d27b27f09cad865035ff3dff4b
Matthew Quijano <mq022002@gmail.com>
2025-01-28 09:35:08 -0500

    feat: Controller `helloWorld`

---
+++ java/demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
