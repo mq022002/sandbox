46a73928e59bd0fbda5c31367e4f1fcae804a70b
Matthew Quijano <mq022002@gmail.com>
2025-02-13 14:22:23 -0500

    feat: Set up Leetcode 217

---
+++ leetcode/arrays-&-hashing/217_contains_duplicate.py
@@ -0,0 +1,26 @@
+from typing import List
+
+"""
+- Need to determine if given list contains any dupes
+- Input, list of numbers being nums
+- Output, boolean. True if there are dupes, false if there are no dupes
+- Iterate through list and check number against all other numbers for every number in the lists
+"""
+
+
+# start of submission
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return False
+
+
+# end of submission
+
+arr1 = [1, 2, 3, 1]
+arr2 = [1, 2, 3, 4]
+arr3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+
+solution = Solution()
+print(solution.containsDuplicate(arr1))
+print(solution.containsDuplicate(arr2))
+print(solution.containsDuplicate(arr3))
