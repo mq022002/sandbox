22fd1f1fb033921e311b3b5912993db107536686
Matthew Quijano <mq022002@gmail.com>
2025-04-18 14:10:04 -0400

    chore: Setup script for entire repository

---
+++ setup.sh
@@ -0,0 +1,43 @@
+echo "==================== START angular-demo ===================="
+cd angular-demo
+npm i
+cd ../
+echo "==================== FINISH angular-demo ===================="
+
+echo "==================== START django-spotify-demo ===================="
+python -m venv .venv
+. .venv/Scripts/activate
+cd django-spotify-demo
+pip install -r requirements.txt
+cd ../
+echo "==================== FINISH django-spotify-demo ===================="
+
+echo "==================== START firebase-functions ===================="
+cd firebase-functions
+npm i
+cd ../
+echo "==================== FINISH firebase-functions ===================="
+
+echo "==================== START nextjs-financial-dashboard ===================="
+cd nextjs-financial-dashboard
+npm i
+cd ../
+echo "==================== FINISH nextjs-financial-dashboard ===================="
+
+echo "==================== START nextjs-progressive-web-app ===================="
+cd nextjs-progressive-web-app
+npm i
+cd ../
+echo "==================== FINISH nextjs-progressive-web-app ===================="
+
+echo "==================== START nextjs-progressive-web-app2 ===================="
+cd nextjs-progressive-web-app2
+npm i
+cd ../
+echo "==================== FINISH nextjs-progressive-web-app2 ===================="
+
+echo "==================== START react-native-to-do-list ===================="
+cd react-native-to-do-list
+npm i
+cd ../
+echo "==================== FINISH react-native-to-do-list ===================="
