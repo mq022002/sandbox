2fe1d45f09dfe7ad207b7faa2b337f7c3be078f2
Matthew Quijano <mq022002@gmail.com>
2025-04-18 17:31:13 -0400

    chore: Stripe publishable key exposed client-side

--- nextjs-pages-router/next.config.ts
+++ nextjs-pages-router/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+  },
 };
 
 export default nextConfig;
